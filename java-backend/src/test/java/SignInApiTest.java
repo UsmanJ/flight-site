c1002f654aa2415166c9e36dd10253d7f1377563
Usman Jamil <usman.jamil@live.co.uk>
2017-02-23 15:59:44 +0000

    Refactored test

--- java-backend/src/test/java/SignInApiTest.java
+++ java-backend/src/test/java/SignInApiTest.java
@@ -1,7 +1,6 @@
 
 import com.google.gson.GsonBuilder;
 import com.usmanjamil.flightsite.model.Auth0SignIn;
-import com.usmanjamil.flightsite.services.UserService;
 
 import org.junit.Before;
 import org.mockito.Mock;
@@ -14,47 +13,34 @@ import org.springframework.web.client.RestTemplate;
 import java.io.InputStream;
 import java.util.Properties;
 
-import static org.mockito.Matchers.anyString;
-
 /**
  * Created by usmanjamil on 23/02/2017.
  */
-public class FirstTest {
-    @Mock
-    private UserService userService;
-
+public class SignInApiTest {
     @Mock
     private RestTemplate restTemplate;
+
+    @Mock
+    private Auth0SignIn user;
 
     @Before
     public void setup(){
         MockitoAnnotations.initMocks(this);
-        this.userService = new UserService();
     }
 
     @Test
-    public void signInTestFromApi() throws Exception {
+    public void testSignInApi() throws Exception {
         Properties prop = new Properties();
-        InputStream input = null;
-
         String filename = "auth0.properties";
-        input = getClass().getClassLoader().getResourceAsStream(filename);
+        InputStream input = getClass().getClassLoader().getResourceAsStream(filename);
 
         prop.load(input);
-        String clientId = prop.getProperty("auth0.clientId");
         String url = prop.getProperty("auth0.signIn");
-        String connection = prop.getProperty("auth0.connection");
-        String scope = prop.getProperty("auth0.scope");
-        String username = "usman.jamil@live.co.uk";
-        String password = "hello1";
 
-        Auth0SignIn user = new Auth0SignIn(clientId, username, password, connection, scope);
-
-        String json = new GsonBuilder().create().toJson(user);
+        String json = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create().toJson(user);
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-
         HttpEntity<String> entity = new HttpEntity<>(json, headers);
 
         Mockito.when(restTemplate
